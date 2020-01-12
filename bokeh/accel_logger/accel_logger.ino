3e854113bfad19d498aa8a1e6dc0d10f7d846555
Andrew Dawes <dawes@pacificu.edu>
2020-01-11 18:08:59 -0800

    updated accel logger to use y data

--- bokeh/accel_logger/accel_logger.ino
+++ bokeh/accel_logger/accel_logger.ino
@@ -2,9 +2,9 @@
 // AMCDawes 2020, for physics dashboarding talk at AAPT WM
 // Based on Hello_Accelerometer and Hello_Buttons examples
 
-// Features: Click left button to record data for LENGTH samples DELAY ms apart
+// Features: Click left button to record y-accel data for LENGTH samples DELAY ms apart
 // Click right button to send data via serial (useful for arduino IDE monitor)
-// TODO: implement dashboard that waits for serial data and plots
+// Works with Bokeh example accel_logger_app 
  
 // Thanks Adafruit!
 
@@ -33,24 +33,24 @@ void loop() {
 
   if (leftButtonPressed) {
     Serial.print("\nArmed: release to begin collection \n");
-    CircuitPlayground.setPixelColor(0, 255,   0,   0);
+    CircuitPlayground.setPixelColor(0, 255, 0, 0); // 1=RED armed
     while( CircuitPlayground.leftButton() ){
       // wait while button is held
       delay(1);
     }
     // begin logging after release
-    CircuitPlayground.setPixelColor(0, 0,   255,   0);
+    CircuitPlayground.setPixelColor(0, 0, 0, 255); // 1=BLUE logging
     Serial.print("\nActive: collecting data \n");
     for (int i = 0; i<LENGTH; i++){
-      data[i] = CircuitPlayground.motionX();
+      data[i] = CircuitPlayground.motionY();
       delay(DELAY);
     }
     Serial.print("\nData saved \n");
-    CircuitPlayground.setPixelColor(9, 0,   255,   0); // Data available
+    CircuitPlayground.setPixelColor(9, 0, 255, 0); // 9=GRN Data available
     CircuitPlayground.setPixelColor(0, 0, 0, 0); // Clear active LED
   }
   if (rightButtonPressed) {
-    CircuitPlayground.setPixelColor(9, 0, 30, 0); // old data still available
+    CircuitPlayground.setPixelColor(9, 0, 30, 0); // old data still available 9=GRN/DIM
     // Read out the data
     Serial.print("DATA\n"); // TODO: do I want these tags?
     for (int i = 0; i<LENGTH; i++){
--- dash/README.md
+++ dash/README.md
@@ -1,6 +1,7 @@
 A set of Dash examples:
 
  - accel_app.py shows accelerometer data from the Circuit Playground Express (in development)
+
  - live_light_app.py shows light levels from the Circuit Playground Express (in development)
 
  
